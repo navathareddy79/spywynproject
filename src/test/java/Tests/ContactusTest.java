e60ec9a6acb36f5cb1a257a0b1cdcb5c7156616e
navathareddy79 <knavatha79@gmail.com>
2025-05-22 12:40:36 -0700

    myfirstgitprogram

---
+++ src/test/java/Tests/ContactusTest.java
@@ -0,0 +1,21 @@
+package Tests;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class ContactusTest {
+  @Test
+  public void tc004_verifytestingcourse() throws Exception {
+	  System.out.println("testingcourse");
+	  WebDriver driver=new ChromeDriver();
+	  
+	  FileInputStream file=new FileInputStream("filepath");  
+	  
+	  HSSFWorkbook  wb=new HSSFWorkbook(file);  
+	  }
+}
---
+++ src/test/java/Tests/HomepageTest.java
@@ -0,0 +1,22 @@
+package Tests;
+
+import org.testng.annotations.Test;
+
+public class HomepageTest {
+  @Test
+  public void tc001_verifyspywynlogo() {
+	  System.out.println("spywynlogo");
+  }
+
+
+@Test
+public void tc001_verifyregisternow() {
+	 System.out.println("registernow");
+}
+
+@Test
+public void tc001_verifyfindyourcourse() {
+	 System.out.println("findyourcourse");
+}
+
+}
